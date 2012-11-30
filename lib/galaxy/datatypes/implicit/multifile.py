e6cc1b1a135efb8101996d35648b594edf914651
John Chilton <jmchilton@gmail.com>
2012-11-29 23:41:13 -0600

    Implement framework for "implicit" datatype loaders and create one to create an implicit composite multi-file datatype for each concrete datatype. This provides all of the advantages of Jorrit Boekel's Adapt (https://bitbucket.org/glormph/adapt) galaxy extensions without requiring explicit definition of special datatypes.
    However, if a multifile version of datatype X exists an implicit multi-file datatype for X will not be created, so none of the flexibility/customiziation allowed by Adapt is lost.

---
+++ lib/galaxy/datatypes/implicit/multifile.py
@@ -0,0 +1,30 @@
+
+from galaxy.datatypes.data import CompositeMultifile
+
+
+class CompositeMultifileDatatypeLoader(object):
+    """
+    This class is responsible CompositeMultifile variants of all explicitly
+    defined datatypes.
+    """
+    def __init__(self, registry):
+        self.registry = registry
+
+    def load(self):
+        """
+        Actually perform building and registering of composite datatypes.
+        """
+        datatypes_by_extension = self.registry.datatypes_by_extension
+        for extension, datatype in dict(datatypes_by_extension).iteritems():
+            multitype_extension = CompositeMultifile.build_multifile_extension(extension)
+            if multitype_extension not in datatypes_by_extension:
+                multi_datatype = self.__build_multi_datatype(extension, datatype)
+                self.__add(multitype_extension, multi_datatype)
+
+    def __build_multi_datatype(self, extension, datatype):
+        multi_datatype = CompositeMultifile(singleton_type=datatype)
+        return multi_datatype
+
+    def __add(self, multitype_extension, multi_datatype):
+        self.registry.datatypes_by_extension[multitype_extension] = multi_datatype
+        self.registry.mimetypes_by_extension[multitype_extension] = multi_datatype.get_mime()
