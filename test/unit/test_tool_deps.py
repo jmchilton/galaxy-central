681e76cd2e06e91ffd508ebdbb5cc4946baf356d
John Chilton <jmchilton@gmail.com>
2013-09-28 22:39:18 -0500

    Tool Dependencies: Implement XML configuration of tool dependency resolver.
    This is largely modelled off of @natefoo's job_conf.xml. This is a minimally configurable XML layout so far, but will be dding more options.
    
    Already can decide between whether to load tool shed installed packages first (default) or manually conifgured ones. The advantages of loading manually configured ones first is outlined here - http://dev.list.galaxyproject.org/Test-Toolshed-Biopython-package-dependency-Atlas-fails-to-install-Was-Re-UnboundLocalError-local-var-tp4661765p4661879.html.

--- test/unit/test_tool_deps.py
+++ test/unit/test_tool_deps.py
@@ -3,6 +3,7 @@ import os.path
 from os import makedirs, symlink
 import galaxy.tools.deps
 from galaxy.util.bunch import Bunch
+from contextlib import contextmanager
 
 
 def touch( fname, data=None ):
@@ -103,3 +104,31 @@ def __build_test_repo(type, version=None):
             installed_changeset_revision=TEST_REPO_CHANGESET
         )
     )
+
+
+def test_parse():
+    with __parse_resolvers('''<dependency_resolvers>
+  <tool_shed_package />
+  <galaxy_package />
+</dependency_resolvers>
+''') as dependency_resolvers:
+        assert 'ToolShed' in dependency_resolvers[0].__class__.__name__
+        assert 'Galaxy' in dependency_resolvers[1].__class__.__name__
+
+    with __parse_resolvers('''<dependency_resolvers>
+  <galaxy_package />
+  <tool_shed_package />
+</dependency_resolvers>
+''') as dependency_resolvers:
+        assert 'Galaxy' in dependency_resolvers[0].__class__.__name__
+        assert 'ToolShed' in dependency_resolvers[1].__class__.__name__
+
+
+@contextmanager
+def __parse_resolvers(xml_content):
+    base_path = tempfile.mkdtemp()
+    f = tempfile.NamedTemporaryFile()
+    f.write(xml_content)
+    f.flush()
+    dm = galaxy.tools.deps.DependencyManager( default_base_path=base_path, conf_file=f.name )
+    yield dm.dependency_resolvers
