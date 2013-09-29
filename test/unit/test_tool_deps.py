bbfddce85a7def82b90a0209bb2bce42dc5cb540
John Chilton <jmchilton@gmail.com>
2013-09-28 23:29:01 -0500

    Tool Dependencies: General clean up of test cases for tool dependencies.

--- test/unit/test_tool_deps.py
+++ test/unit/test_tool_deps.py
@@ -1,54 +1,46 @@
 import tempfile
 import os.path
 from os import makedirs, symlink
-import galaxy.tools.deps
+from shutil import rmtree
+from galaxy.tools.deps import DependencyManager
 from galaxy.util.bunch import Bunch
 from contextlib import contextmanager
 
 
-def touch( fname, data=None ):
-    f = open( fname, 'w' )
-    if data:
-        f.write( data )
-    f.close()
-
-
 def test_tool_dependencies():
     # Setup directories
-    base_path = tempfile.mkdtemp()
-    # mkdir( base_path )
-    for name, version, sub in [ ( "dep1", "1.0", "env.sh" ), ( "dep1", "2.0", "bin" ), ( "dep2", "1.0", None ) ]:
-        if sub == "bin":
-            p = os.path.join( base_path, name, version, "bin" )
-        else:
-            p = os.path.join( base_path, name, version )
-        try:
-            makedirs( p )
-        except:
-            pass
-        if sub == "env.sh":
-            touch( os.path.join( p, "env.sh" ) )
+    with __test_base_path() as base_path:
+        for name, version, sub in [ ( "dep1", "1.0", "env.sh" ), ( "dep1", "2.0", "bin" ), ( "dep2", "1.0", None ) ]:
+            if sub == "bin":
+                p = os.path.join( base_path, name, version, "bin" )
+            else:
+                p = os.path.join( base_path, name, version )
+            try:
+                makedirs( p )
+            except:
+                pass
+            if sub == "env.sh":
+                __touch( os.path.join( p, "env.sh" ) )
 
-    dm = galaxy.tools.deps.DependencyManager( default_base_path=base_path )
+        dm = DependencyManager( default_base_path=base_path )
+        d1_script, d1_path, d1_version = dm.find_dep( "dep1", "1.0" )
+        assert d1_script == os.path.join( base_path, 'dep1', '1.0', 'env.sh' )
+        assert d1_path == os.path.join( base_path, 'dep1', '1.0' )
+        assert d1_version == "1.0"
+        d2_script, d2_path, d2_version = dm.find_dep( "dep1", "2.0" )
+        assert d2_script == None
+        assert d2_path == os.path.join( base_path, 'dep1', '2.0' )
+        assert d2_version == "2.0"
 
-    d1_script, d1_path, d1_version = dm.find_dep( "dep1", "1.0" )
-    assert d1_script == os.path.join( base_path, 'dep1', '1.0', 'env.sh' )
-    assert d1_path == os.path.join( base_path, 'dep1', '1.0' )
-    assert d1_version == "1.0"
-    d2_script, d2_path, d2_version = dm.find_dep( "dep1", "2.0" )
-    assert d2_script == None
-    assert d2_path == os.path.join( base_path, 'dep1', '2.0' )
-    assert d2_version == "2.0"
+        ## Test default versions
+        symlink( os.path.join( base_path, 'dep1', '2.0'), os.path.join( base_path, 'dep1', 'default' ) )
+        default_script, default_path, default_version = dm.find_dep( "dep1", None )
+        assert default_version == "2.0"
 
-    ## Test default versions
-    symlink( os.path.join( base_path, 'dep1', '2.0'), os.path.join( base_path, 'dep1', 'default' ) )
-    default_script, default_path, default_version = dm.find_dep( "dep1", None )
-    assert default_version == "2.0"
-
-    ## Test default will not be fallen back upon by default
-    default_script, default_path, default_version = dm.find_dep( "dep1", "2.1" )
-    assert default_script == None
-    assert default_version == None
+        ## Test default will not be fallen back upon by default
+        default_script, default_path, default_version = dm.find_dep( "dep1", "2.1" )
+        assert default_script == None
+        assert default_version == None
 
 
 TEST_REPO_USER = "devteam"
@@ -69,27 +61,34 @@ TEST_REPO = Bunch(
 
 def test_toolshed_set_enviornment():
     # Setup directories
-    base_path = tempfile.mkdtemp()
-    test_repo = __build_test_repo('set_environment')
-    dm = galaxy.tools.deps.DependencyManager( default_base_path=base_path )
-    env_settings_dir = os.path.join(base_path, "environment_settings", TEST_REPO_NAME, TEST_REPO_USER, TEST_REPO_NAME, TEST_REPO_CHANGESET)
-    os.makedirs(env_settings_dir)
-    d1_script, d1_path, d1_version = dm.find_dep( TEST_REPO_NAME, version=None, type='set_environment', installed_tool_dependencies=[test_repo] )
-    assert d1_version == None
-    assert d1_script == os.path.join(env_settings_dir, "env.sh"), d1_script
+    with __test_base_path() as base_path:
+        test_repo = __build_test_repo('set_environment')
+        dm = DependencyManager( default_base_path=base_path )
+        env_settings_dir = os.path.join(base_path, "environment_settings", TEST_REPO_NAME, TEST_REPO_USER, TEST_REPO_NAME, TEST_REPO_CHANGESET)
+        os.makedirs(env_settings_dir)
+        d1_script, d1_path, d1_version = dm.find_dep( TEST_REPO_NAME, version=None, type='set_environment', installed_tool_dependencies=[test_repo] )
+        assert d1_version == None
+        assert d1_script == os.path.join(env_settings_dir, "env.sh"), d1_script
 
 
 def test_toolshed_package():
     # Setup directories
-    base_path = tempfile.mkdtemp()
-    test_repo = __build_test_repo('package', version=TEST_VERSION)
-    dm = galaxy.tools.deps.DependencyManager( default_base_path=base_path )
-    package_dir = os.path.join(base_path, TEST_REPO_NAME, TEST_VERSION, TEST_REPO_USER, TEST_REPO_NAME, TEST_REPO_CHANGESET)
-    os.makedirs(package_dir)
-    touch(os.path.join(package_dir, 'env.sh'))
-    d1_script, d1_path, d1_version = dm.find_dep( TEST_REPO_NAME, version=TEST_VERSION, type='package', installed_tool_dependencies=[test_repo] )
-    assert d1_version == TEST_VERSION, d1_version
-    assert d1_script == os.path.join(package_dir, "env.sh"), d1_script
+    with __test_base_path() as base_path:
+        test_repo = __build_test_repo('package', version=TEST_VERSION)
+        dm = DependencyManager( default_base_path=base_path )
+        package_dir = os.path.join(base_path, TEST_REPO_NAME, TEST_VERSION, TEST_REPO_USER, TEST_REPO_NAME, TEST_REPO_CHANGESET)
+        os.makedirs(package_dir)
+        __touch(os.path.join(package_dir, 'env.sh'))
+        d1_script, d1_path, d1_version = dm.find_dep( TEST_REPO_NAME, version=TEST_VERSION, type='package', installed_tool_dependencies=[test_repo] )
+        assert d1_version == TEST_VERSION, d1_version
+        assert d1_script == os.path.join(package_dir, "env.sh"), d1_script
+
+
+def __touch( fname, data=None ):
+    f = open( fname, 'w' )
+    if data:
+        f.write( data )
+    f.close()
 
 
 def __build_test_repo(type, version=None):
@@ -148,9 +147,18 @@ def test_parse():
 
 @contextmanager
 def __parse_resolvers(xml_content):
+    with __test_base_path() as base_path:
+        f = tempfile.NamedTemporaryFile()
+        f.write(xml_content)
+        f.flush()
+        dm = DependencyManager( default_base_path=base_path, conf_file=f.name )
+        yield dm.dependency_resolvers
+
+
+@contextmanager
+def __test_base_path():
     base_path = tempfile.mkdtemp()
-    f = tempfile.NamedTemporaryFile()
-    f.write(xml_content)
-    f.flush()
-    dm = galaxy.tools.deps.DependencyManager( default_base_path=base_path, conf_file=f.name )
-    yield dm.dependency_resolvers
+    try:
+        yield base_path
+    finally:
+        rmtree(base_path)
