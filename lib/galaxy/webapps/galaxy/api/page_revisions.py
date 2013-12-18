872a892a6bcd04654810d8d064985eea326ef62f
John Chilton <jmchilton@gmail.com>
2013-12-18 00:56:50 -0600

    Add API functional tests for Kyle's page API methods.
    Reworks a lot of things in the page API, tightens up security, uses new exception based error codes and status codes, etc...

--- lib/galaxy/webapps/galaxy/api/page_revisions.py
+++ lib/galaxy/webapps/galaxy/api/page_revisions.py
@@ -2,65 +2,65 @@
 API for updating Galaxy Pages
 """
 import logging
-from galaxy import web
+from galaxy.web import _future_expose_api as expose_api
+from galaxy import exceptions
 from galaxy.web.base.controller import SharableItemSecurityMixin, BaseAPIController, SharableMixin
-from galaxy.model.search import GalaxySearchEngine
 from galaxy.model.item_attrs import UsesAnnotations
-from galaxy.exceptions import ItemAccessibilityException
-from galaxy.util.sanitize_html import sanitize_html
 
 log = logging.getLogger( __name__ )
 
+
 class PageRevisionsController( BaseAPIController, SharableItemSecurityMixin, UsesAnnotations, SharableMixin ):
 
-    @web.expose_api
+    @expose_api
     def index( self, trans, page_id, **kwd ):
-        r = trans.sa_session.query( trans.app.model.PageRevision ).filter_by( page_id=trans.security.decode_id(page_id) )
+        page = self._get_page( trans, page_id )
+        self._verify_page_ownership( trans, page )
         out = []
-        for page in r:
-            if self.security_check( trans, page, True, True ):
-                out.append( self.encode_all_ids( trans, page.to_dict(), True) )
+        for page_revision in page.revisions:
+            out.append( self.encode_all_ids( trans, page_revision.to_dict(), True) )
         return out
 
-
-    @web.expose_api
+    @expose_api
     def create( self, trans, page_id, payload, **kwd ):
         """
         payload keys:
             page_id
             content
         """
-        user = trans.get_user()
-        error_str = ""
-        
-        if not page_id:
-            error_str = "page_id is required"
-        elif not payload.get("content", None):
-            error_str = "content is required"
+        content = payload.get("content", None)
+        if not content:
+            raise exceptions.ObjectAttributeMissingException("content undefined or empty")
+
+        page = self._get_page( trans, page_id )
+        self._verify_page_ownership( trans, page )
+
+        if 'title' in payload:
+            title = payload['title']
         else:
+            title = page.title
 
-            # Create the new stored page
+        page_revision = trans.app.model.PageRevision()
+        page_revision.title = title
+        page_revision.page = page
+        page.latest_revision = page_revision
+        page_revision.content = payload.get("content", "")
+        # Persist
+        session = trans.sa_session
+        session.flush()
+
+        return page_revision.to_dict( view="element" )
+
+    def _get_page( self, trans, page_id ):
+        page = None
+        try:
             page = trans.sa_session.query( trans.app.model.Page ).get( trans.security.decode_id(page_id) )
-            if page is None:
-                return { "error" : "page not found"}
-            
-            if not self.security_check( trans, page, True, True ):
-                return { "error" : "page not found"}
+        except Exception:
+            pass
+        if not page:
+            raise exceptions.ObjectNotFound()
+        return page
 
-            if 'title' in payload:
-                title = payload['title']
-            else:
-                title = page.title
-
-            page_revision = trans.app.model.PageRevision()
-            page_revision.title = title
-            page_revision.page = page
-            page.latest_revision = page_revision
-            page_revision.content = payload.get("content", "")
-            # Persist
-            session = trans.sa_session
-            session.flush()
-
-            return {"success" : "revision posted"}
-        
-        return { "error" : error_str }    
+    def _verify_page_ownership( self, trans, page ):
+        if not self.security_check( trans, page, True, True ):
+            raise exceptions.ItemOwnershipException()
--- lib/galaxy/webapps/galaxy/api/pages.py
+++ lib/galaxy/webapps/galaxy/api/pages.py
@@ -2,27 +2,29 @@
 API for updating Galaxy Pages
 """
 import logging
-from galaxy import web
+from galaxy import exceptions
+from galaxy.web import _future_expose_api as expose_api
 from galaxy.web.base.controller import SharableItemSecurityMixin, BaseAPIController, SharableMixin
-from galaxy.model.search import GalaxySearchEngine
 from galaxy.model.item_attrs import UsesAnnotations
-from galaxy.exceptions import ItemAccessibilityException
 from galaxy.util.sanitize_html import sanitize_html
 
 log = logging.getLogger( __name__ )
 
+
 class PagesController( BaseAPIController, SharableItemSecurityMixin, UsesAnnotations, SharableMixin ):
 
-    @web.expose_api
-    def index( self, trans, deleted='False', **kwd ):
-        r = trans.sa_session.query( trans.app.model.Page )
+    @expose_api
+    def index( self, trans, **kwd ):
+        # Dropping deleted, not implemented yet, adding filter by user.
+        # TODO: implemented deleted on/off switch.
+        # TODO: Also show pages shared with user. (Public pages?)
+        r = trans.sa_session.query( trans.app.model.Page ).filter_by( user=trans.user )
         out = []
         for row in r:
             out.append( self.encode_all_ids( trans, row.to_dict(), True) )
         return out
 
-
-    @web.expose_api
+    @expose_api
     def create( self, trans, payload, **kwd ):
         """
         payload keys:
@@ -32,61 +34,66 @@ class PagesController( BaseAPIController, SharableItemSecurityMixin, UsesAnnotat
             annotation
         """
         user = trans.get_user()
-        error_str = ""
-        
+
         if not payload.get("title", None):
-            error_str = "Page name is required"
+            raise exceptions.ObjectAttributeMissingException( "Page name is required" )
         elif not payload.get("slug", None):
-            error_str = "Page id is required"
+            raise exceptions.ObjectAttributeMissingException( "Page id is required" )
         elif not self._is_valid_slug( payload["slug"] ):
-            error_str = "Page identifier must consist of only lowercase letters, numbers, and the '-' character"
+            raise exceptions.ObjectAttributeInvalidException( "Page identifier must consist of only lowercase letters, numbers, and the '-' character" )
         elif trans.sa_session.query( trans.app.model.Page ).filter_by( user=user, slug=payload["slug"], deleted=False ).first():
-            error_str = "Page id must be unique"
-        else:
-            # Create the new stored page
-            page = trans.app.model.Page()
-            page.title = payload['title']
-            page.slug = payload['slug']
-            page_annotation = sanitize_html( payload.get("annotation",""), 'utf-8', 'text/html' )
-            self.add_item_annotation( trans.sa_session, trans.get_user(), page, page_annotation )
-            page.user = user
-            # And the first (empty) page revision
-            page_revision = trans.app.model.PageRevision()
-            page_revision.title = payload['title']
-            page_revision.page = page
-            page.latest_revision = page_revision
-            page_revision.content = payload.get("content", "")
-            # Persist
-            session = trans.sa_session
-            session.add( page )
-            session.flush()
+            raise exceptions.DuplicatedSlugException( "Page slug must be unique" )
+        # Create the new stored page
+        page = trans.app.model.Page()
+        page.title = payload['title']
+        page.slug = payload['slug']
+        page_annotation = sanitize_html( payload.get("annotation", ""), 'utf-8', 'text/html' )
+        self.add_item_annotation( trans.sa_session, trans.get_user(), page, page_annotation )
+        page.user = user
+        # And the first (empty) page revision
+        page_revision = trans.app.model.PageRevision()
+        page_revision.title = payload['title']
+        page_revision.page = page
+        page.latest_revision = page_revision
+        page_revision.content = payload.get("content", "")
+        # Persist
+        session = trans.sa_session
+        session.add( page )
+        session.flush()
 
-            rval = self.encode_all_ids( trans, page.to_dict(), True) 
-            return rval
-        
-        return { "error" : error_str }    
+        rval = self.encode_all_ids( trans, page.to_dict(), True )
+        return rval
 
-
-    @web.expose_api
+    @expose_api
     def delete( self, trans, id, **kwd ):
-        page_id = id;
+        page_id = id
+
+        page = None
         try:
             page = trans.sa_session.query(self.app.model.Page).get(trans.security.decode_id(page_id))
-        except Exception, e:
-            return { "error" : "Page with ID='%s' can not be found\n Exception: %s" % (page_id, str( e )) }
+        except Exception:
+            pass
+
+        if not page:
+            raise exceptions.ObjectNotFound()
 
         # check to see if user has permissions to selected workflow
         if page.user != trans.user and not trans.user_is_admin():
-            return { "error" : "Workflow is not owned by or shared with current user" }
+            raise exceptions.ItemOwnershipException( )
 
         #Mark a workflow as deleted
         page.deleted = True
         trans.sa_session.flush()
-        return {"success" : "Deleted", "id" : page_id}
+        return ''  # TODO: Figure out what to return on DELETE, document in guidelines!
 
-    @web.expose_api
-    def show( self, trans, id, deleted='False', **kwd ):
+    @expose_api
+    def show( self, trans, id, **kwd ):
+        # Deleted not implemented for now, so removed from args.
         page = trans.sa_session.query( trans.app.model.Page ).get( trans.security.decode_id( id ) )
-        rval = self.encode_all_ids( trans, page.to_dict(), True) 
+
+        if page.user != trans.get_user():
+            raise exceptions.ItemOwnershipException()
+
+        rval = self.encode_all_ids( trans, page.to_dict(), True )
         rval['content'] = page.latest_revision.content
         return rval
