f9b3e3fef17498c3f49b89be8ec73ab9129acdaf
John Chilton <jmchilton@gmail.com>
2014-01-21 15:39:05 -0600

    Dataset collections - implicit "map" creates collections.
    When running normal tools with normal data inputs across dataset collections in parallel - a collection will be created for each output with a "structure" matching that of the input (for instance pairs will create pairs, lists will create lists with the same identifiers, etc...). A previous changeset added the ability to run the tool in parallel - this changeset extends that functionality to create analogous collections from these parallel runs.
    
    For example, if one filters a pair of FASTQ files a pair is created out of the result. Likewise, if one filters a list of FASTQ files - a list collection with same cardinality is built from the results.
    
    There is a lot left TODO here - for one a lot of this logic should be moved into the dataset_collection module. The matching needs to be exact right now - not a problem for pairs (every 'element' has name 'left' or 'right') but for lists with element names - these have to match exactly - but a 'list' like samp1_l, samp2_l, samp3_l should be able to match against samp1_r, samp2_r, samp3_r and create a new list samp1, samp2, and samp3. Even if there is no matching prefixes a new 'unlabeled' list should be able to be created.

--- lib/galaxy/tools/__init__.py
+++ lib/galaxy/tools/__init__.py
@@ -1872,7 +1872,7 @@ class Tool( object, Dictifiable ):
         # Fixed set of input parameters may correspond to any number of jobs.
         # Expand these out to individual parameters for given jobs (tool
         # executions).
-        expanded_incomings = self.__expand_meta_properties( trans, incoming )
+        expanded_incomings, collection_info = self.__expand_meta_properties( trans, incoming )
 
         if not expanded_incomings:
             raise exceptions.MessageException( "Tool execution failed, trying to run a tool over an empty collection." )
@@ -1920,7 +1920,7 @@ class Tool( object, Dictifiable ):
                 template_vars = dict( errors=errors, tool_state=state, incoming=incoming, error_message=error_message )
             # If we've completed the last page we can execute the tool
             elif all_pages or state.page == self.last_page:
-                execution_tracker = execute( trans, self, all_params, history=history, rerun_remap_job_id=rerun_remap_job_id )
+                execution_tracker = execute( trans, self, all_params, history=history, rerun_remap_job_id=rerun_remap_job_id, collection_info=collection_info )
                 if execution_tracker.successful_jobs:
                     template = 'tool_executed.mako'
                     template_vars = dict( out_data=execution_tracker.output_datasets, num_jobs=len( execution_tracker.successful_jobs ), job_errors=execution_tracker.execution_errors )
@@ -2047,6 +2047,8 @@ class Tool( object, Dictifiable ):
             else:
                 return input_classification.SINGLE, incoming[ input_key ]
 
+        matched_collections = {}
+
         def collection_classifier( input_key ):
             multirun_key = "%s|__collection_multirun__" % input_key
             if multirun_key in incoming:
@@ -2054,6 +2056,7 @@ class Tool( object, Dictifiable ):
                 hdc_id = self.app.security.decode_id( encoded_hdc_id )
                 hdc = trans.sa_session.query( model.HistoryDatasetCollectionAssociation ).get( hdc_id )
                 hdas = map( lambda dce: dce.hda, hdc.collection.elements )
+                matched_collections[ input_key ] = hdc
                 return input_classification.MATCHED, hdas
             else:
                 return input_classification.SINGLE, incoming[ input_key ]
@@ -2075,7 +2078,7 @@ class Tool( object, Dictifiable ):
         collection_multirun_found = False
         for key, value in incoming.iteritems():
             multirun_found = try_replace_key( key, "|__multirun__" ) or multirun_found
-            collection_multirun_found = try_replace_key( key, "|__collection_multirun__" )
+            collection_multirun_found = try_replace_key( key, "|__collection_multirun__" ) or collection_multirun_found
 
         if multirun_found and collection_multirun_found:
             # In theory doable, but to complicated for a first pass.
@@ -2083,9 +2086,14 @@ class Tool( object, Dictifiable ):
             raise exceptions.ToolMetaParameterException( message )
 
         if multirun_found:
-            return expand_multi_inputs( incoming_template, classifier )
+            return expand_multi_inputs( incoming_template, classifier ), None
         else:
-            return expand_multi_inputs( incoming_template, collection_classifier )
+            expanded_incomings = expand_multi_inputs( incoming_template, collection_classifier )
+            if matched_collections:
+                collection_info = self.app.dataset_collections_service.match_collections( matched_collections )
+            else:
+                collection_info = None
+            return expanded_incomings, collection_info
 
     def find_fieldstorage( self, x ):
         if isinstance( x, FieldStorage ):
