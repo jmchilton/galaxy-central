3ac0e0f6fda3bc53a95f0c687c2170f0bc235360
John Chilton <jmchilton@gmail.com>
2014-02-02 10:32:19 -0600

    Dataset collections - workflows - allow data_collection params.
    Extend workflow running, extraction, viewing to allow tools with data_collection parameters.
    
    This is brutally ugly because "isinstance( input, DataToolParameter )" which was already not very Pythonic has been updated to be "isinstance( input, DataToolParameter ) or isinstance( input, DataCollectionToolParameter )". This should all be reworked with an "is_data_parameter" on base ToolParameter.

--- lib/galaxy/webapps/galaxy/api/workflows.py
+++ lib/galaxy/webapps/galaxy/api/workflows.py
@@ -204,6 +204,7 @@ class WorkflowsAPIController(BaseAPIController, UsesStoredWorkflowMixin, UsesHis
                 history = self.get_history( trans, from_history_id, check_ownership=False, check_accessible=True )
                 job_ids = map( trans.security.decode_id, payload.get( "job_ids", [] ) )
                 dataset_ids = map( trans.security.decode_id, payload.get( "dataset_ids", [] ) )
+                dataset_collection_ids = map( trans.security.decode_id, payload.get( "dataset_collection_ids", [] ) )
                 workflow_name = payload[ "workflow_name" ]
                 stored_workflow = extract_workflow(
                     trans=trans,
@@ -211,6 +212,7 @@ class WorkflowsAPIController(BaseAPIController, UsesStoredWorkflowMixin, UsesHis
                     history=history,
                     job_ids=job_ids,
                     dataset_ids=dataset_ids,
+                    dataset_collection_ids=dataset_collection_ids,
                     workflow_name=workflow_name,
                 )
                 item = stored_workflow.to_dict( value_mapper={ "id": trans.security.encode_id } )
