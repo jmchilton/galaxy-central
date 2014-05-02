bbcd9bcd493762a32675c612ac116ac2814ebbd9
John Chilton <jmchilton@gmail.com>
2014-05-02 11:45:46 -0500

    Rework new validation module for Carl's changes in c4f468e43b93406804bd7665a2755b16f8bac463.
    Also delegate controller base methods to module to eliminate code duplication.

--- lib/galaxy/util/validation.py
+++ lib/galaxy/util/validation.py
@@ -2,22 +2,35 @@
 
 TODO: Refactor BaseController references to similar methods to use this module.
 """
+from galaxy import exceptions
 from galaxy.util.sanitize_html import sanitize_html
 
 
 def validate_and_sanitize_basestring( key, val ):
     if not isinstance( val, basestring ):
-        raise ValueError( '%s must be a string or unicode: %s' % ( key, str( type( val ) ) ) )
+            raise exceptions.RequestParameterInvalidException( '%s must be a string or unicode: %s'
+                                                               % ( key, str( type( val ) ) ) )
     return unicode( sanitize_html( val, 'utf-8', 'text/html' ), 'utf-8' )
 
 
 def validate_and_sanitize_basestring_list( key, val ):
-    if not isinstance( val, list ):
-        raise ValueError( '%s must be a list: %s' % ( key, str( type( val ) ) ) )
-    return [ unicode( sanitize_html( t, 'utf-8', 'text/html' ), 'utf-8' ) for t in val ]
+    try:
+        assert isinstance( val, list )
+        return [ unicode( sanitize_html( t, 'utf-8', 'text/html' ), 'utf-8' ) for t in val ]
+    except ( AssertionError, TypeError ):
+        raise exceptions.RequestParameterInvalidException( '%s must be a list of strings: %s'
+                                                           % ( key, str( type( val ) ) ) )
 
 
 def validate_boolean( key, val ):
     if not isinstance( val, bool ):
-        raise ValueError( '%s must be a boolean: %s' % ( key, str( type( val ) ) ) )
+        raise exceptions.RequestParameterInvalidException( '%s must be a boolean: %s'
+                                                           % ( key, str( type( val ) ) ) )
     return val
+
+
+#TODO:
+#def validate_integer( self, key, val, min, max ):
+#def validate_float( self, key, val, min, max ):
+#def validate_number( self, key, val, min, max ):
+#def validate_genome_build( self, key, val ):
