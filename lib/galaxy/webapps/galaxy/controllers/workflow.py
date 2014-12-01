65fa3258ba645b7ce63cdaec6d50f212c7b94f51
John Chilton <jmchilton@gmail.com>
2014-12-01 15:08:58 -0500

    Sanitize workflow names during workflow copy and delete.

--- lib/galaxy/webapps/galaxy/controllers/workflow.py
+++ lib/galaxy/webapps/galaxy/controllers/workflow.py
@@ -556,7 +556,7 @@ class WorkflowController( BaseUIController, SharableMixin, UsesStoredWorkflowMix
         session.add( new_stored )
         session.flush()
         # Display the management page
-        trans.set_message( 'Created new workflow with name "%s"' % new_stored.name )
+        trans.set_message( 'Created new workflow with name "%s"' % escape( new_stored.name ) )
         return self.list( trans )
 
     @web.expose
@@ -603,7 +603,7 @@ class WorkflowController( BaseUIController, SharableMixin, UsesStoredWorkflowMix
         trans.sa_session.add( stored )
         trans.sa_session.flush()
         # Display the management page
-        trans.set_message( "Workflow '%s' deleted" % stored.name )
+        trans.set_message( "Workflow '%s' deleted" % escape( stored.name ) )
         return self.list( trans )
 
     @web.expose
