33b910de24289c7e005375e68d7306dd66fde3df
John Chilton <jmchilton@gmail.com>
2014-12-20 15:21:40 -0500

    Introduce abstraction to prevent lib/galaxy/workflow/modules.py from needing tools_by_id.

--- lib/galaxy/tools/__init__.py
+++ lib/galaxy/tools/__init__.py
@@ -511,6 +511,20 @@ class ToolBox( object, Dictifiable ):
             return[ self.tools_by_id[ tool_id ] ]
         return None
 
+    def get_tool_id( self, tool_id ):
+        """ Take a tool id (potentially from a different Galaxy instance or that
+        is no longer loaded  - and find the closest match to the currently loaded
+        tools (using get_tool for inexact matches).
+        """
+        if tool_id not in self.tools_by_id:
+            tool = self.get_tool( tool_id )
+            if tool:
+                tool_id = tool.id
+            else:
+                tool_id = None
+        # else exact match - leave unmodified.
+        return tool_id
+
     def get_loaded_tools_by_lineage( self, tool_id ):
         """Get all loaded tools associated by lineage to the tool whose id is tool_id."""
         tool_lineage = self.lineage_map.get( tool_id )
